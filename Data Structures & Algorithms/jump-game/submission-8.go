3dc9690fd473e28d5851c0faffe86c0dc8b7a8e9
SandhyaGanesh <49909198+SandhyaGanesh@users.noreply.github.com>
2026-08-27 12:18:48 +0530

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.go
@@ -0,0 +1,18 @@
+func canJump(nums []int) bool {
+    l := len(nums)
+    goal := l - 1
+    hops := 0
+
+    for hops <= l - 1 {
+        if goal == 0 {
+            return true
+        }
+        hops += 1
+        if goal - hops >= 0 && nums[goal-hops] >= hops {
+            goal = goal-hops
+            hops = 0
+        }
+        
+    }
+    return false
+}
